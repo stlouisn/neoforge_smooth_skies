dee9d6801ad422f46140b41febccd64d5794eb81
MicrocontrollersDev <microcontrollersyt@gmail.com>
2024-08-24 11:34:12 -0700

    provides smooth skies and clear void

--- src/main/java/dev/microcontrollers/smoothskies/SmoothSkies.java
+++ src/main/java/dev/microcontrollers/smoothskies/SmoothSkies.java
@@ -2,10 +2,17 @@ package dev.microcontrollers.smoothskies;
 
 import dev.microcontrollers.smoothskies.config.SmoothConfig;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SmoothSkies implements ModInitializer {
+    final Logger logger = LoggerFactory.getLogger("Smooth Skies");
+
     @Override
     public void onInitialize() {
         SmoothConfig.CONFIG.load();
+        if (FabricLoader.getInstance().isModLoaded("clearvoid")) logger.warn("Clear Void mod has been disabled as Smooth Skies replaces it.");
+        if (FabricLoader.getInstance().isModLoaded("clear-skies")) logger.warn("Clear Skies mod has been disabled as Smooth Skies replaces it.");
     }
 }
