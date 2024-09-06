3cee33ea5877e25b155d80bae17ad881d36b61a1
stlouisn <stlouisn@hotmail.com>
2024-09-06 14:55:23 -0300

    misc fixes

--- src/main/java/dev/smoothskies/SmoothSkies.java
+++ src/main/java/dev/smoothskies/SmoothSkies.java
@@ -1,14 +1,13 @@
 package dev.smoothskies;
 
 import dev.smoothskies.config.ModConfig;
-
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@SuppressWarnings("unused")
 public class SmoothSkies implements ModInitializer {
 
-  @SuppressWarnings("unused")
   public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
 
   public void onInitialize() {
