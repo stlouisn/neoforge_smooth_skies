4f8100d0981f8b5f3cd1f3caed52bb8ecda1b769
stlouisn <stlouisn@hotmail.com>
2024-09-11 12:41:31 -0300

    Update ModConfig.java

--- src/main/java/dev/smoothskies/config/ModConfig.java
+++ src/main/java/dev/smoothskies/config/ModConfig.java
@@ -1,1 +1,23 @@
 package dev.smoothskies.config;
+
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.ConfigHolder;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Jankson;
+import net.minecraft.util.Identifier;
+
+public final class ModConfig {
+
+  public final static ModConfigData CONFIG;
+
+  static {
+    Jankson jankson = new Jankson.Builder().registerSerializer(Identifier.class, ((identifier, marshaller) -> marshaller.serialize(identifier.toString())))
+                                           .registerDeserializer(String.class, Identifier.class, (object, marshaller) -> Identifier.of(object))
+                                           .build();
+    ConfigHolder<ModConfigData> configHolder = AutoConfig.register(ModConfigData.class, (config, configClass) -> new JanksonConfigSerializer<>(config, configClass, jankson));
+    CONFIG = configHolder.getConfig();
+  }
+
+  public static void init() {
+  }
+}
