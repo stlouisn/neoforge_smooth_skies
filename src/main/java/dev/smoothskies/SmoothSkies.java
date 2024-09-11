d361ed5279a3cbf8a296226b5e1e9a11cb230772
stlouisn <stlouisn@hotmail.com>
2024-09-11 12:20:38 -0300

    Update SmoothSkies.java

--- src/main/java/dev/smoothskies/SmoothSkies.java
+++ src/main/java/dev/smoothskies/SmoothSkies.java
@@ -1,16 +1,33 @@
 package dev.smoothskies;
 
-import dev.smoothskies.config.ModConfig;
-import net.fabricmc.api.ModInitializer;
+import dev.smoothskies.config.SmoothSkiesConfigData;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public final class SmoothSkies implements ModInitializer {
+@SuppressWarnings("unused")
+@Mod(Constants.MOD_ID)
+public final class SmoothSkies {
 
-  @SuppressWarnings("unused")
   public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
 
-  public void onInitialize() {
-    ModConfig.init();
+  public SmoothSkies(IEventBus modBus) {
+    modBus.addListener(this::commonSetup);
+    modBus.addListener(this::clientSetup);
+    ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> (client, parent) -> AutoConfig.getConfigScreen(SmoothSkiesConfigData.class, parent).get());
+  }
+
+  private void commonSetup(FMLCommonSetupEvent event) {
+    event.enqueueWork(SmoothSkiesConfig::init);
+  }
+
+  private void clientSetup(FMLClientSetupEvent event) {
+    event.enqueueWork(SmoothSkiesClient::init);
   }
 }
