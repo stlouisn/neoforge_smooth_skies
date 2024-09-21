9357af928be93f0bf481af9cfd6efac1e72aab9f
stlouisn <stlouisn@hotmail.com>
2024-09-21 11:53:00 -0300

    added ConfigHandler file.commments

--- src/main/java/dev/smoothskies/config/ConfigHandler.java
+++ src/main/java/dev/smoothskies/config/ConfigHandler.java
@@ -7,20 +7,24 @@ public final class ConfigHandler {
 
   public static final ModConfigSpec configSpec;
 
-  public static ConfigValue<Boolean> lowerSkyVoidDarkness, clearSkies;
   public static ConfigValue<Integer> distance;
+  public static ConfigValue<Boolean> lowerSkyVoidDarkness;
+  public static ConfigValue<Boolean> clearSkies;
 
   static {
 
     ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
 
     distance = builder
+        .comment("Set a custom view distance for the skybox fog. Higher numbers mean bluer skies.\nDefault: 96")
         .defineInRange("distance", 96, 0, 550);
 
     lowerSkyVoidDarkness = builder
+        .comment("Lowers the required height for the sky to turn black to the world's lowest height.\nThis prevents the sky turning black at a high Y value.\nDefault: true")
         .define("lowerSkyVoidDarkness", true);
 
     clearSkies = builder
+        .comment("Removes banding on the horizon, making the sky one uniform color.\nDefault: false")
         .define("clearSkies", false);
 
     configSpec = builder.build();
